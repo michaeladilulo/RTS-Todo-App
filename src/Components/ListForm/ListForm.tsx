e47339b7953ef14e3466ecc9d06c8cf4a5ef6e78
michaeladilulo <michaeladilulo@gmail.com>
2024-08-09 12:05:51 -0600

    Removes Import Statements

--- src/Components/ListForm/ListForm.tsx
+++ src/Components/ListForm/ListForm.tsx
@@ -1,8 +1,6 @@
 import React, { FC } from 'react'
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
