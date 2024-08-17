9ce9c458f28b0eb4a0c1a1669a4ca1bdb770542b
michaeladilulo <michaeladilulo@gmail.com>
2024-08-17 16:28:50 -0600

    Created an order of tasks for stubbing out the task form and what I need to have happen

---
+++ src/Components/TaskForm/TaskForm.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './TaskForm.css';
+
+const TaskForm = () => {
+    return (
+        <>
+        {/* TODO: This will just be the way I stub it out now to get it to work, can refactor and talk to somebody about it later on */}
+        {/* Create a checkbox in the top right of the list card that has a label of 'Create Tasks for lists' */}
+        {/* When Checked => This form will show */}
+        {/* When Not Checked => This form will not show */}
+        {/* This checkbox when checked will run an api call that has an endpoint of List/${listId} */}
+        {/* Once that list id is obtained, then I can add it to the URL for the post request for the tasks */}
+        {/* The task URL will be /list/${listId}/task */}
+        {/* Upon creation of the task you will see something in the database where the list is tied to the task */}
+        {/* Will probably need to create a type for Task */}
+        </>
+    )
+}
+
+export default TaskForm;
